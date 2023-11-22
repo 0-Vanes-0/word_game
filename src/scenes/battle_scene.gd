7f922acae869e5c5219a4a021aa53ea5e6a830a1
Ivan <malyshev.ivan@gmail.com>
2023-11-22 11:33:13 +0100

    Today's plan

--- src/scenes/battle_scene.gd
+++ src/scenes/battle_scene.gd
@@ -29,22 +29,5 @@ func _process(delta: float) -> void:
 	$Background/ParallaxBackground/ParallaxLayer2.motion_offset += Vector2.RIGHT * 20 * delta
 
 
-#func _test_sprites():
-#	var vectors: Array[Vector2] = [
-#		Vector2(100, 480),
-#		Vector2(200, 480),
-#		Vector2(300, 480),
-#		Vector2(600, 480),
-#		Vector2(700, 480),
-#		Vector2(800, 480),
-#	]
-#	for i in 6:
-#		var sprite := AnimatedSprite2D.new()
-#		sprite.sprite_frames = preload("res://game_resources/sprite_frames/knight_sprite_frames.tres")
-#		$Battlers.add_child(sprite)
-#		sprite.play("default")
-#		sprite.position = vectors[i]
-
-
 func _on_back_button_pressed() -> void:
 	Global.switch_to_scene(Preloader.game_scene)
