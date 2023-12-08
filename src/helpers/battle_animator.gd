06c13af452363854cb81d4def7cf40c6bfbb1253
Ivan <malyshev.ivan@gmail.com>
2023-12-08 21:08:57 +0100

    Animating group attack

--- src/helpers/battle_animator.gd
+++ src/helpers/battle_animator.gd
@@ -25,21 +25,37 @@ func animate_turn(target_group: Array[Battler] = []):
 	var current_battler_scale := Vector2(current_battler.scale)
 	var current_battler_offset := Vector2(current_battler.sprite.offset)
 	
-	var target_battler: Battler = battle_scene.battlers[battle_manager.target_battler_index]
-	var target_battler_index := int(target_battler.index)
-	var target_battler_position := Vector2(target_battler.position)
-	var target_battler_scale := Vector2(target_battler.scale)
-	var target_battler_offset := Vector2(target_battler.sprite.offset)
+	var one: Array[Battler] = [ battle_scene.battlers[battle_manager.target_battler_index] ]
+	var target_battlers: Array[Battler] = (
+			target_group if not target_group.is_empty()
+			else one
+	)
+	var target_battler_indexes: Array[int] #int(target_battler.index)
+	var target_battler_positions: Array[Vector2]# = Vector2(target_battler.position)
+	var target_battler_scales: Array[Vector2]#Vector2(target_battler.scale)
+	var target_battler_offsets: Array[Vector2]#Vector2(target_battler.sprite.offset)
 	
-	if current_battler_index != target_battler_index:
-		battle_scene.battlers_node.move_child(target_battler, -1)
-		target_battler.scale *= 2.0
+	target_battler_indexes.resize(target_battlers.size())
+	target_battler_positions.resize(target_battlers.size())
+	target_battler_scales.resize(target_battlers.size())
+	target_battler_offsets.resize(target_battlers.size())
+	for i in target_battlers.size():
+		target_battler_indexes[i] = target_battlers[i].index
+		target_battler_positions[i] = target_battlers[i].position
+		target_battler_scales[i] = target_battlers[i].scale
+		target_battler_offsets[i] = target_battlers[i].sprite.offset
+	
+	if current_battler_index != target_battler_indexes[0]: # TODO: плохая проверка, надо проверять весь массив, но лучше отдельной функцией
+		for b in target_battlers:
+			battle_scene.battlers_node.move_child(b, -1)
+			b.scale *= 2.0
 	battle_scene.battlers_node.move_child(current_battler, -1)
 	current_battler.scale *= 2.0
 	
 	var left_position := Vector2.RIGHT * Global.SCREEN_WIDTH / 3
 	var right_position := Vector2.RIGHT * Global.SCREEN_WIDTH * 2 / 3
 	var center_position := Vector2.RIGHT * Global.SCREEN_WIDTH / 2
+	var group_gap := Vector2.RIGHT * Global.SCREEN_WIDTH * 1 / 8
 	
 	var black := battle_scene.black_screen
 	black.self_modulate.a = 0.5
@@ -50,14 +66,16 @@ func animate_turn(target_group: Array[Battler] = []):
 	current_battler.tokens_container.hide()
 	current_battler.selection.hide()
 	current_battler.selection_hover.hide()
-	target_battler.health_bar.hide()
-	target_battler.tokens_container.hide()
-	target_battler.selection.hide()
-	target_battler.selection_hover.hide()
+	for b in target_battlers:
+		b.health_bar.hide()
+		b.tokens_container.hide()
+		b.selection.hide()
+		b.selection_hover.hide()
 	
 	current_battler.anim_action(current_action_type)
-	if current_battler_index != target_battler_index:
-		target_battler.anim_reaction(current_action_type)
+	if current_battler_index != target_battler_indexes[0]:
+		for b in target_battlers:
+			b.anim_reaction(current_action_type)
 	
 	#if effect_type > 0:
 		#battle_scene.effect_sprite.offset = (battle_scene.effect_sprite.sprite_frames as MySpriteFrames).separate_offsets[Rune.get_type_string(effect_type)]
@@ -73,17 +91,19 @@ func animate_turn(target_group: Array[Battler] = []):
 	
 	
 	_tween = _new_tween()
-	if current_battler_index != target_battler_index:
+	if current_battler_index != target_battler_indexes[0]:
 		_tween.tween_property(
 				current_battler, "position",
-				left_position if current_battler_index < target_battler_index else right_position,
+				left_position if current_battler_index < target_battler_indexes[0] else right_position,
 				0.0
 		)
-		_tween.tween_property(
-				target_battler, "position",
-				right_position if current_battler_index < target_battler_index else left_position,
-				0.0
-		)
+		for i in target_battlers.size():
+			_tween.tween_property(
+					target_battlers[i], "position",
+					right_position + group_gap * i if current_battler_index < target_battlers[i].index else left_position,
+					0.0
+			)
+			print(right_position - group_gap + group_gap * i)
 		if current_action_type == Battler.ActionTypes.ATTACK and not _is_attack_ranged(current_battler.type):
 			_tween.tween_property(
 					current_battler, "position",
@@ -132,31 +152,32 @@ func animate_turn(target_group: Array[Battler] = []):
 					current_battler.tokens_container.show()
 	)
 				
-	if current_battler_index != target_battler_index:
-		_tween.parallel().tween_property(
-				target_battler, "position",
-				target_battler_position,
-				0.25
-		)
-		_tween.parallel().tween_property(
-				target_battler, "scale",
-				target_battler_scale,
-				0.25
-		)
-		_tween.parallel().tween_callback(
-				func():
-					target_battler.sprite.offset = target_battler_offset
-					if target_battler.is_alive:
-						target_battler.anim_idle()
-					else:
-						target_battler.anim_die()
-		)
-		_tween.tween_callback(
-				func():
-					if target_battler.is_alive:
-						target_battler.health_bar.show()
-						target_battler.tokens_container.show()
-		)
+	if current_battler_index != target_battler_indexes[0]:
+		for i in target_battlers.size():
+			_tween.parallel().tween_property(
+					target_battlers[i], "position",
+					target_battler_positions[i],
+					0.25
+			)
+			_tween.parallel().tween_property(
+					target_battlers[i], "scale",
+					target_battler_scales[i],
+					0.25
+			)
+			_tween.parallel().tween_callback(
+					func():
+						target_battlers[i].sprite.offset = target_battler_offsets[i]
+						if target_battlers[i].is_alive:
+							target_battlers[i].anim_idle()
+						else:
+							target_battlers[i].anim_die()
+			)
+			_tween.tween_callback(
+					func():
+						if target_battlers[i].is_alive:
+							target_battlers[i].health_bar.show()
+							target_battlers[i].tokens_container.show()
+			)
 	
 	_tween.tween_callback(
 			func():
